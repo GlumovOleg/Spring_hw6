8ab5486447f1ae8086c0829a6b38cbdc72bcfa90
Oleg <darek2587@gmail.com>
2024-02-15 15:33:22 +0300

    create Performer

---
+++ src/main/java/geekbrains/glumov_oleg/PerformerRepository.java
@@ -0,0 +1,9 @@
+package geekbrains.glumov_oleg;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PerformerRepository extends JpaRepository<Performer, Long> {
+
+}
